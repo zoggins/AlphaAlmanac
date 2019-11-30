8929796e2569eff06ad5c648a39069e40d323eba
Zoggins <zoggins@gmail.com>
2019-11-29 17:04:31 -0800

    Auto building of Doc Brown formatted folders

---
+++ Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphaAlmanac
+{
+
+    struct data
+    {
+        public string title;
+        public string old_dir;
+
+    };
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var directory_data = new List<data>();
+
+            Directory.SetCurrentDirectory("..");
+            var directories = Directory.GetDirectories(Directory.GetCurrentDirectory());
+            foreach (var dir in directories)
+            {
+                if (Path.GetFileName(dir) != "01")
+                {
+                    var files = Directory.GetFiles(dir);
+                    bool foundTitle = false;
+                    foreach (var file in files)
+                    {
+                        if (Path.GetFileName(file) == "title.txt")
+                        {
+                            var title = File.ReadAllText(Path.Combine(dir, file));
+                            var item = new data() ;
+                            item.title = title;
+                            Directory.Move(dir, dir + ".temp");
+                            item.old_dir = dir + ".temp";
+                            directory_data.Add(item);
+                            foundTitle = true;
+                        }
+                    }
+
+                    if (!foundTitle)
+                    {
+                        var title = Path.GetFileName(dir);
+                        System.IO.File.WriteAllText(Path.Combine(dir, "title.txt"), title);
+                        var item = new data();
+                        item.title = title;
+                        Directory.Move(dir, dir + ".temp");
+                        item.old_dir = dir + ".temp";
+                        directory_data.Add(item);
+                    }
+                }
+            }
+
+            directory_data.Sort((s1, s2) => s1.title.ToLower().CompareTo(s2.title.ToLower()));
+
+            int i = 2;
+            foreach(var item in directory_data)
+            {
+                Directory.Move(item.old_dir, Path.Combine(Directory.GetCurrentDirectory(), i < 100 ? i.ToString("00") : i.ToString("000")));
+                ++i;
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("AlphaAlmanac")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AlphaAlmanac")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f03d3a60-5492-4bb4-90fe-ae8ea076e928")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
